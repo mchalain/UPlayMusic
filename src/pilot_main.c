12813bd423db51dbf102552f4439b61858014928
mchalain <marc.chalain@gmail.com>
2014-10-08 15:17:12 +0200

    set default value for the device

--- src/pilot_main.c
+++ src/pilot_main.c
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
 
 #include "config.h"
 #include "pilot_atk.h"
 #include "pilot_mods.h"
 #include "mupnp.h"
+
+#define DEFAULT_NAME "UPlayMusic"
+#define DEFAULT_UUID "UPlayMusic"
 
 static struct pilot_application *g_application;
 static void *g_upnp;
@@ -46,6 +53,88 @@ upme_check(char *line, int len)
 	return ret;
 }
 
+static char *
+create_udn (char *interface)
+{
+  int sock = -1;
+  char *buf;
+  unsigned char *ptr;
+
+#if (defined(BSD) || defined(__FreeBSD__) || defined(__APPLE__))
+  int mib[6];
+  size_t len;
+  struct if_msghdr *ifm;
+  struct sockaddr_dl *sdl;
+#else /* Linux */
+  struct ifreq ifr;
+#endif
+
+  if (!interface)
+    return NULL;
+
+#if (defined(BSD) || defined(__FreeBSD__) || defined(__APPLE__))
+  mib[0] = CTL_NET;
+  mib[1] = AF_ROUTE;
+  mib[2] = 0;
+  mib[3] = AF_LINK;
+  mib[4] = NET_RT_IFLIST;
+
+  mib[5] = if_nametoindex (interface);
+  if (mib[5] == 0)
+  {
+    perror ("if_nametoindex");
+    return NULL;
+  }
+
+  if (sysctl (mib, 6, NULL, &len, NULL, 0) < 0)
+  {
+    perror ("sysctl");
+    return NULL;
+  }
+
+  buf = malloc (len);
+  if (sysctl (mib, 6, buf, &len, NULL, 0) < 0)
+  {
+    perror ("sysctl");
+    return NULL;
+  }
+
+  ifm = (struct if_msghdr *) buf;
+  sdl = (struct sockaddr_dl*) (ifm + 1);
+  ptr = (unsigned char *) LLADDR (sdl);
+#else /* Linux */
+  /* determine UDN according to MAC address */
+  sock = socket (AF_INET, SOCK_STREAM, 0);
+  if (sock < 0)
+  {
+    perror ("socket");
+    return NULL;
+  }
+
+  strcpy (ifr.ifr_name, interface);
+  strcpy (ifr.ifr_hwaddr.sa_data, "");
+
+  if (ioctl (sock, SIOCGIFHWADDR, &ifr) < 0)
+  {
+    perror ("ioctl");
+    return NULL;
+  }
+
+  buf = (char *) malloc (64 * sizeof (char));
+  memset (buf, 0, 64);
+  ptr = (unsigned char *) ifr.ifr_hwaddr.sa_data;
+#endif /* (defined(BSD) || defined(__FreeBSD__)) */
+
+  snprintf (buf, 64, "%s-%02x%02x%02x%02x%02x%02x", DEFAULT_UUID,
+            (ptr[0] & 0377), (ptr[1] & 0377), (ptr[2] & 0377),
+            (ptr[3] & 0377), (ptr[4] & 0377), (ptr[5] & 0377));
+
+  if (sock)
+    close (sock);
+
+  return buf;
+}
+
 int main(int argc, const char **argv)
 {
 	int ret;
@@ -61,15 +150,21 @@ int main(int argc, const char **argv)
 	value = pilot_application_getopt_string(g_application, "name");
 	if (value)
 		g_cmd_name = value;
+  if (!g_cmd_name)
+    g_cmd_name = strdup (DEFAULT_NAME);
 	value = pilot_application_getopt_string(g_application, "uuid");
 	if (value)
 		g_cmd_uuid = value;
+  if (!g_cmd_uuid)
+    g_cmd_uuid = create_udn (g_cmd_ip_address);
 	value = pilot_application_getopt_string(g_application, "serial");
 	if (value)
 		g_cmd_serial = value;
 	value = pilot_application_getopt_string(g_application, "ip_address");
 	if (value)
 		g_cmd_ip_address = value;
+  if (!g_cmd_ip_address)
+    g_cmd_ip_address = strdup ("eth0");
 	integer = pilot_application_getopt_int(g_application, "listen_port");
 	if (integer)
 		g_cmd_listen_port = integer;
