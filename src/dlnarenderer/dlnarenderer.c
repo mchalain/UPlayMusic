84cee705d3ca1844fc304a01a95c473ba00c1100
mchalain <marc.chalain@gmail.com>
2014-10-08 15:17:59 +0200

    support of libdlna 0.4.2

--- src/dlnarenderer/dlnarenderer.c
+++ src/dlnarenderer/dlnarenderer.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 
-#include <dlna.h>
+#include <upnp/dlna.h>
 
 extern const dlna_profiler_t mpg123_profiler;
 extern int mpg123_profiler_init ();
@@ -20,20 +20,13 @@ struct upnp * upnp_start(char *name, char *uuid, char *serial_number, char *ip_a
         dlna_t *dlna;
         dlna_device_t *device;
         const dlna_profiler_t *profiler;
-        dlna_org_flags_t flags;
         dlna_capability_mode_t cap;
 
-        flags = DLNA_ORG_FLAG_STREAMING_TRANSFER_MODE |
-                DLNA_ORG_FLAG_BACKGROUND_TRANSFERT_MODE |
-                DLNA_ORG_FLAG_CONNECTION_STALL |
-                DLNA_ORG_FLAG_DLNA_V15;
         cap = DLNA_CAPABILITY_DLNA;
 
         /* init DLNA stack */
         dlna = dlna_init ();
-        dlna_set_org_flags (dlna, flags);
         dlna_set_verbosity (dlna, DLNA_MSG_INFO);
-        dlna_set_capability_mode (dlna, cap);
         dlna_set_extension_check (dlna, 1);
 
         /* init Media profiler */
@@ -42,15 +35,22 @@ struct upnp * upnp_start(char *name, char *uuid, char *serial_number, char *ip_a
         dlna_add_profiler (dlna, profiler);
 
         /* define NIC to be used */
+        if (!ip_address)
+          ip_address = strdup ("eth0");
         dlna_set_interface (dlna, ip_address);
         dlna_set_port (dlna, listen_port);
 
         /* set some UPnP device properties */
-        device = dlna_device_new ();
+        device = dlna_device_new (cap);
         dlna_device_set_type (device, DLNA_DEVICE_TYPE_DMR,"DMR");
         dlna_device_set_friendly_name (device, name);
         dlna_device_set_uuid (device, uuid);
         dlna_device_set_serial_number (device, serial_number);
+        dlna_device_set_model_description (device, "UPlayMusic : DLNA Media Renderer");
+        dlna_device_set_model_name (device, name);
+        dlna_device_set_model_number (device, "001");
+        dlna_device_set_manufacturer (device, "Marc Chalain");
+        dlna_device_set_manufacturer_url (device, "http://github.com/mchalain/UPlayMusic");
 
         dlna_service_register (device, cms_service_new(dlna));
         dlna_service_register (device, rcs_service_new(dlna));
